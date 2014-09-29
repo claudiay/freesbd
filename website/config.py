89a942b359fef94e38b2e37895b99f2f23ce2fb9
Claudia Yadathi <cy@norse-corp.com>
2014-09-28 23:50:55 -0700

    initial .po files for translations

---
+++ website/config.py
@@ -0,0 +1,10 @@
+
+LANGUAGES = ["en_US.ISO8859-1", "bn_BD.UTF-8", "da_DK.ISO8859-1", "de_DE.ISO8859-1",
+    "el_GR.ISO8859-7", "es_ES.ISO8859-1", "fr_FR.ISO8859-1", "hu_HU.ISO8859-2",
+    "it_IT.ISO8859-15", "ja_JP.eucJP", "mn_MN.UTF-8", "nl_NL.ISO8859-1", "pl_PL.ISO8859-2",
+    "pt_BR.ISO8859-1", "ru_RU.KOI8-R", "sr_YU.ISO8859-2", "tr_TR.ISO8859-9", "zh_CN.UTF-8",
+    "zh_TW.UTF-8"]
+
+LANGS = [l.split('.')[0] for l in LANGUAGES]
+
+
